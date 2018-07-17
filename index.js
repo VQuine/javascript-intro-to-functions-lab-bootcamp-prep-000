ae096a44a80ed9ce2a0d1301bbd2f45cb92db8a1
Kimberly Aviles <01010110.v@gmail.com>
2018-07-17 07:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
-
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
-
-
-
-
-
-function shout(string) {
-  return string.toUpperCase()
-}
-
-function logShout(string) {
-  console.log(`${string.toUpperCase()}`)
-}
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logWhisper(string) {
-  console.log(`${string.toLowerCase()}`)
-}
-
-
-
-
-
-if function (sayHiToGrandma(string)) === lowercase {
-  return (`I can't hear you!`);
-}
-  else if function (sayHiToGrandma(string)) === uppercase {
-  return (`YES INDEED!`);
-}
-  else function (sayHiToGrandma(string)) === mixedCase {
-  return (`I love you, too.`);
+if (sayHiToGrandma() === uppercase) {
+  return 'YES INDEED!';
+} else if sayHiToGrandma() === lowercase {
+  return 'I can't hear you!';
+} else {
+  return 'I love you, too!';
 }
