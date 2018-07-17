ba36e1ca93d2d4becfa7653391d4ffedd09c8256
Kimberly Aviles <01010110.v@gmail.com>
2018-07-17 07:45:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (sayHiToGrandma() === uppercase) {
-  return 'YES INDEED!';
+  return "YES INDEED!";
 } else if sayHiToGrandma() === lowercase {
-  return 'I can't hear you!';
+  return "I can't hear you!";
 } else {
-  return 'I love you, too!';
+  return "I love you, too!";
 }
